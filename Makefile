90c2275889a74c2cc6dcc03afff76c052865c6ab
kilych <kilych@zoho.com>
2020-03-26 20:38:27 +0300

    Add 3rd step

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 install:
-	/usr/bin/env npm install
+	npm install
 
 start:
-	/usr/bin/env node bin/brain-games.js
+	node bin/brain-games.js
 
 publish:
-	/usr/bin/env npm publish --dry-run
+	npm publish --dry-run
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import readlineSync from 'readline-sync';
+
+const run = () => {
+  console.log('Welcome to the Brain Games!\n');
+
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+};
+
+export default run;
