93e182f1325b95bf08e3fef8e354b11a2ca2234f
kilych <kilych@zoho.com>
2020-04-17 10:54:39 +0300

    Improve naming in calculator game

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -2,7 +2,7 @@ import { getRandomInt } from '../math.js';
 
 const maxNum = 20;
 
-const operators = {
+const signToOperationMap = {
   '+': (a, b) => a + b,
   '-': (a, b) => a - b,
   '*': (a, b) => a * b,
@@ -10,22 +10,22 @@ const operators = {
 
 const description = 'What is the result of the expression?';
 
-const getRandomOperatorSymbol = () => {
-  const operatorSymbols = Object.keys(operators);
-  const random = getRandomInt(0, operatorSymbols.length - 1);
+const getRandomOperationSign = () => {
+  const operationSigns = Object.keys(signToOperationMap);
+  const random = getRandomInt(0, operationSigns.length - 1);
 
-  return operatorSymbols[random];
+  return operationSigns[random];
 };
 
 const makeQuestion = () => [
-  getRandomOperatorSymbol(),
+  getRandomOperationSign(),
   getRandomInt(1, maxNum),
   getRandomInt(1, maxNum),
 ];
 
-const stringifyQuestion = ([symbol, operand1, operand2]) => `${operand1} ${symbol} ${operand2}`;
+const stringifyQuestion = ([operationSign, operand1, operand2]) => `${operand1} ${operationSign} ${operand2}`;
 
-const makeAnswer = ([symbol, ...operands]) => operators[symbol](...operands).toString();
+const makeAnswer = ([sign, ...operands]) => signToOperationMap[sign](...operands).toString();
 
 const makeQuestionAndAnswer = () => {
   const question = makeQuestion();
