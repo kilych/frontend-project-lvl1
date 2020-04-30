7d6fad464004a2dc444288b2f82e4d77f1dba7bf
kilych <kilych@zoho.com>
2020-04-30 21:24:08 +0300

    Simplify getRandomInt

--- src/math.js
+++ src/math.js
@@ -1,8 +1,6 @@
 const getRandomInt = (min, max) => {
-  const ceiledMin = Math.ceil(min);
-  const flooredMax = Math.floor(max);
-
-  return Math.floor(ceiledMin + (Math.random() * (flooredMax - ceiledMin + 1)));
+  const random = min + (Math.random() * (max - min + 1));
+  return Math.floor(random);
 };
 
 const isEven = (num) => num % 2 === 0;
